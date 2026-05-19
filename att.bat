ac1db5fee9dc4fe89e8cca9efc619d8269d7cb30
Victor Almeida Nunes <victor.n@amo.com.br>
2026-05-19 13:46:36 -0300

    19/05/2026 13:46:36,39

---
+++ att.bat
@@ -0,0 +1,16 @@
+@echo off
+cd /d "C:\Users\victor.n\PROJETO"
+
+echo ============================
+echo ENVIANDO ATUALIZACOES...
+echo ============================
+
+git add .
+
+git commit -m "%date% %time%"
+
+git push origin main
+
+echo.
+echo FINALIZADO!
+pause
