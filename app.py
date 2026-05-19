ab9381856ec52456164ead221191368853d9615c
Victor Almeida Nunes <victor.n@amo.com.br>
2026-05-19 17:36:07 -0300

    19/05/2026 17:36:07,00

--- app.py
+++ app.py
@@ -579,7 +579,7 @@ for p in pastas_para_ler:
 
                         pdf_viewer(
                             caminho_arquivo,
-                            width="70%",
+                            width="90%",
                             height=900
                         )
 
