a576bc9585da528b80fd06fdf64b0eff9e748c3e
Victor Almeida Nunes <victor.n@amo.com.br>
2026-05-19 11:33:51 -0300

    att

--- app.py
+++ app.py
@@ -372,7 +372,7 @@ for pauta_nome, lista_fornecedores in fornecedores_por_pauta.items():
         unsafe_allow_html=True
     )
 
-    quantidade_colunas = 5gi
+    quantidade_colunas = 5
 
     colunas = st.columns(
         quantidade_colunas
