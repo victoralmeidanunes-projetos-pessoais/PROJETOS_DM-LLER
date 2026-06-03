2b5aeb9d342206c90bdabe8bdeb188b4ece04928
Victor Almeida Nunes <victor.n@amo.com.br>
2026-06-03 10:34:33 -0300

    03/06/2026 10:34:33,26

---
+++ att - Copia (2).bat
@@ -0,0 +1,13 @@
+
+cd /d "C:\Users\victor.n\PROJETO"
+
+echo ============================
+echo ENVIANDO ATUALIZACOES...
+echo ============================
+
+git add .
+
+git commit -m "%date% %time%"
+
+git push
+
