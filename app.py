8eac56eae4280875b66716f036609d3eeb287881
Victor Almeida Nunes <victor.n@amo.com.br>
2026-06-03 10:27:32 -0300

    03/06/2026 10:27:31,99

--- app.py
+++ app.py
@@ -145,7 +145,7 @@ if os.path.exists(PASTA_RAIZ):
 # =========================================
 # CABEÇALHO
 # =========================================
-st.text(" ")
+
 st.header("CAMPANHAS ATIVAS")
 
 st.caption(
