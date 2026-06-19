bcdadfd72337ee2e8996a43f0758a362418b8eb4
Victor Almeida Nunes <victor.n@amo.com.br>
2026-06-19 16:35:29 -0300

    19/06/2026 16:35:29,03

--- supabase_config.py
+++ supabase_config.py
@@ -1,7 +1,6 @@
 from supabase import create_client
 
 SUPABASE_URL = "https://yruwsicscttnphjvrjhc.supabase.co"
-
 SUPABASE_KEY = "sb_publishable_cMclO4izvQ1jzD3TzXXzGg_lSKDMsod"
 
 supabase = create_client(
